5515b27697334a7a2736c4fb5a490a67c0c4e07e
Azhar Khan <azhar.khan@peak.ai>
2020-10-01 21:07:28 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setup(
     name='eks_token',
-    version='0.0.1c0',
+    version='0.1.0',
     author='Peak AI',
     author_email='support@peak.ai',
     description='EKS Token package, an alternate to "aws eks get-token ..." CLI',
