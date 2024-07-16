c3201ac1100410fb7d8719a54f3faf678d542646
Azhar Khan <azhar.khan@peak.ai>
2024-07-16 10:04:22 +0530

    chore: upgrade version for new release

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as readme:
 
 setup(
     name='eks_token',
-    version='0.2.0',
+    version='0.2.1',
     author='Peak AI',
     author_email='support@peak.ai',
     description='EKS Token package, an alternate to "aws eks get-token ..." CLI',
